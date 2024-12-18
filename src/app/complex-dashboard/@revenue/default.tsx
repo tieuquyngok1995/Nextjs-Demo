3816d7daa3f22394849acd5e9fdd04ee39f7687d
Vu Quoc Tuan <tuan-vq@fujinet.net>
2024-12-18 10:14:38 +0700

    c

---
+++ src/app/complex-dashboard/@revenue/default.tsx
@@ -0,0 +1,7 @@
+import Card from "@/components/card";
+
+export default function DefaultRevenueMetrics() {
+  return (
+    <Card>Revenue Metrics</Card>
+  );
+}
---
+++ src/app/complex-dashboard/default.tsx
@@ -0,0 +1,5 @@
+export default function DefaultComplexDashboardPage() {
+  return (
+    <h1>Complex dashboard</h1>
+  );
+}
